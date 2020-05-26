a6ff81cf6d7e0f393e787fda83da89837227029e
Edward Horsey <edwardhorsey@me.com>
2020-05-26 08:26:55 +0100

    added regex

--- index.js
+++ index.js
@@ -20,15 +20,18 @@ return {
 
 
 const getTempo = function(){
+  const regex = /^\d+$/
   if (event.keyCode == 13) {
     event.preventDefault()
     let tempo = document.querySelectorAll('form')[0][0].value
+    if (regex.test(tempo) && tempo > 0 && tempo < 501) {
     let milliseconds = dataCalculation(tempo)
-    console.log(milliseconds)
+    // console.log(milliseconds)
     for (prop in milliseconds) {
       let tableItem = document.getElementById(prop)
       tableItem.innerHTML = `${milliseconds[prop]} ms`
     }
-
+  } else {document.getElementById('alert').innerHTML = 'Please enter a number between 1-500'}
   }
 }
+
