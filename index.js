56c81bd0b6b10eba5ab4ca034ac50795db485b50
Edward Horsey <edwardhorsey@me.com>
2020-06-02 17:04:03 +0100

    fixed couple display bugs

--- index.js
+++ index.js
@@ -3,54 +3,63 @@ const slider = document.getElementById('slider');
 slider.addEventListener('click', () => {
   let sliderValue = slider.value;
   printTable(sliderValue);
-})
+});
 
+slider.addEventListener('mousemove', () => {
+  let sliderValue = slider.value;
+  printTable(sliderValue);
+});
 
 const dataCalculation = function(temp){
-  console.log(`what up`, temp)
-let quarterNote = 60000/temp;
-console.log(this);
-return {
-  quarter: Math.round(quarterNote),
-  quarterT: Math.round(quarterNote/3 * 2),
-  eigth: Math.round(quarterNote/2),
-  eigthDot: Math.round(quarterNote/2 * 1.5),
-  eigthT: Math.round(quarterNote/6 * 2),
-  sixteenth: Math.round(quarterNote/4),
-  sixteenthDot: Math.round(quarterNote/4 * 1.5),
-  sixteenthT: Math.round(quarterNote/12 * 2),
-}
-}
+  console.log(`what up`, temp);
+  let quarterNote = 60000/temp;
+  return {
+    quarter: Math.round(quarterNote),
+    quarterT: Math.round(quarterNote/3 * 2),
+    eigth: Math.round(quarterNote/2),
+    eigthDot: Math.round(quarterNote/2 * 1.5),
+    eigthT: Math.round(quarterNote/6 * 2),
+    sixteenth: Math.round(quarterNote/4),
+    sixteenthDot: Math.round(quarterNote/4 * 1.5),
+    sixteenthT: Math.round(quarterNote/12 * 2),
+  }
+};
 
 // console.log(dataCalculation(120))
 
 const printTable = function(value){
 
-  let milliseconds = dataCalculation(value)
+  let milliseconds = dataCalculation(value);
   
   for (prop in milliseconds) {
-    let tableItem = document.getElementById(prop)
-    tableItem.innerHTML = `${milliseconds[prop]} ms`
+    let tableItem = document.getElementById(prop);
+    tableItem.innerHTML = `${milliseconds[prop]} ms`;
   }
 
   slider.value = value;
   document.getElementById('tempo').value = value;
   document.getElementById('bpm').innerHTML = `BPM: ${value}`;
-}
+};
 
 
 const getTempo = function(){
   if (event.keyCode == 13) {
-    event.preventDefault()
-    // gather tempo
-    let tempo = document.querySelectorAll('form')[0][0].value
+    event.preventDefault();
+    let tempo = document.querySelectorAll('form')[0][0].value;
     const regex = /^\d+$/
-    if (regex.test(tempo) && tempo > 0 && tempo < 301) { // put the numbers in the regex
-       printTable(tempo);
+    if (
+      regex.test(tempo)
+      && tempo > 0
+      && tempo < 301
+      ) {
+        document.getElementById('alert').innerHTML = '';
+        printTable(tempo);
+        document.getElementById('resultBlock').style.display = 'block';
       } else {
-        document.getElementById('alert').innerHTML = 'Please enter a number between 1-300'
+        document.getElementById('alert').innerHTML = 'Please enter a number between 1-300';
+        document.getElementById('resultBlock').style.display = 'none';
       }
-      document.getElementById('resultBlock').style.display = 'block';
   }
-}
+};
 
+// put the numbers in the regex?? Not sure it's worth it..
---
+++ tests.js
@@ -0,0 +1,4 @@
+const regex = /^[1-300]+$/
+
+
+console.log(regex.test());
