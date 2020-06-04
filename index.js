1f79273fab934780f236382f3d77003e62238231
Edward Horsey <edwardhorsey@me.com>
2020-06-04 08:08:31 +0100

    added year

--- index.js
+++ index.js
@@ -80,3 +80,6 @@ const getTempo = () => {
 };
 
 // put the numbers in the regex?? Not sure it's worth it..
+
+const theYear = new Date;
+document.getElementById('year').innerHTML += ` ${theYear.getFullYear()}`;
